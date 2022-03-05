3f178dec89a2a37c1be172dc545c9299815352ea
Anshuman Panda <anshumanpanda211@gmail.com>
2022-03-05 14:31:33 +0530

    .

--- adr.py
+++ adr.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def adr_order(valebook, valbzbook):
     # i = -1
     # j = -1
@@ -15,14 +17,19 @@ def adr_order(valebook, valbzbook):
     valebook_sell = valebook[1]
     valbzbook_buy = valbzbook[0]
     valbzbook_sell = valbzbook[1]
+    
     if len(valebook_buy) > 0 and len(valbzbook_buy) > 0 and len(valbzbook_sell) > 0 and len(valebook_sell) > 0:
-        if valebook_buy[-1][0] > valbzbook_sell[-1][0] + 1:
-            orders.append({type: "add", "symbol": "VALBZ", "dir": "BUY", "price": valbzbook_sell[-1][0] + 1, "size": 10})
+        bzsell = np.minimum(valbzbook_sell[0])
+        lesell = np.minimum(valebook_sell[0])
+        bzbuy = np.minimum(valbzbook_buy[0])
+        lebuy = np.minimum(valebook_buy[0])
+        if lebuy > bzsell + 1:
+            orders.append({type: "add", "symbol": "VALBZ", "dir": "BUY", "price": bzsell + 1, "size": 10})
             orders.append({type: "convert", "symbol": "VALBZ", "dir": "SELL", "size": 10 })
-            orders.append({type: "add", "symbol": "VALE", "dir": "SELL", "price": valebook_buy[-1][0] - 1, "size": 10 })
+            orders.append({type: "add", "symbol": "VALE", "dir": "SELL", "price": lebuy - 1, "size": 10 })
 
-        elif valbzbook_buy[-1][0] > valebook_sell[-1][0] + 1:
-            orders.append({type: "add", "symbol": "VALE", "dir": "BUY", "price": valebook_sell[-1][0] + 1, "size": 10 })
+        elif bzbuy > lesell + 1:
+            orders.append({type: "add", "symbol": "VALE", "dir": "BUY", "price": lesell + 1, "size": 10 })
             orders.append({type: "convert", "symbol": "VALE", "dir": "SELL", "size": 10 })
-            orders.append({type: "add", "symbol": "VALBZ", "dir": "SELL", "price": valbzbook_buy[-1][0] - 1, "size": 10 })
+            orders.append({type: "add", "symbol": "VALBZ", "dir": "SELL", "price": bzbuy - 1, "size": 10 })
     return orders
